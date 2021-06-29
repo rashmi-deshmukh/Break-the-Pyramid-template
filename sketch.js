274858a2885ebeb1a731f753f8b684bd5b9f9255
Rashmi Deshmukh <57807833+rashmi-deshmukh@users.noreply.github.com>
2021-06-29 19:21:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
-var star, starBody;
+var ground , greenBall;
+var stone1, stone2, stone3, stone4, stone5, stone6;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -13,28 +12,38 @@ function setup() {
 	world = engine.world;
 
 	//creating the ground
-	ground=Bodies.rectangle(400 , 740 , 800 ,10, {restitution:0.5, isStatic:true});
+	var groundOptions={
+		restitution:0.5, isStatic:true
+	}
+	ground=Bodies.rectangle(400 , 730 , 800 ,10, groundOptions);
 	World.add(world, ground);
 
 	//creating the green ball
-	ball1 = Bodies.circle(400 , 100 , 40, {restitution:1, isStatic:true});
-	World.add(world, ball1);
+	var greenballOptions={
+		restitution:1.5, isStatic:true
+	}
+	greenBall = Bodies.circle(400 , 100 , 40, greenballOptions);
+	World.add(world, greenBall);
 	
+	
+	var stoneOptions={
+		restitution:0.2, isStatic:false
+	}
 	//creating the pyramid
-	square1 = Bodies.rectangle(400 , 700 , 50 , 50, {restitution:1, isStatic:false});
-	square2 = Bodies.rectangle(470 , 700 , 50 , 50, {restitution:1, isStatic:false});
-	square3 = Bodies.rectangle(330 , 700 , 50 , 50, {restitution:1, isStatic:false});
-	square4 = Bodies.rectangle(370 , 650 , 50 , 50, {restitution:1, isStatic:false});
-	square5 = Bodies.rectangle(440 , 650 , 50 , 50, {restitution:1, isStatic:false});
-	square6 = Bodies.rectangle(400 , 600 , 50 , 50, {restitution:1, isStatic:false});
+	stone1 = Bodies.rectangle(400 , 700 , 50 , 50, stoneOptions);
+	stone2 = Bodies.rectangle(470 , 700 , 50 , 50, stoneOptions);
+	stone3 = Bodies.rectangle(330 , 700 , 50 , 50, stoneOptions);
+	stone4 = Bodies.rectangle(370 , 650 , 50 , 50, stoneOptions);
+	stone5 = Bodies.rectangle(440 , 650 , 50 , 50, stoneOptions);
+	stone6 = Bodies.rectangle(400 , 600 , 50 , 50, stoneOptions);
 	
 	//Adding the pyramids to the world
-	World.add(world, square1);
-	World.add(world, square2);
-	World.add(world, square3);
-	World.add(world, square4);
-	World.add(world, square5);
-	World.add(world, square6);
+	World.add(world, stone1);
+	World.add(world, stone2);
+	World.add(world, stone3);
+	World.add(world, stone4);
+	World.add(world, stone5);
+	World.add(world, stone6);
 }
 
 
@@ -42,32 +51,30 @@ function draw() {
   Engine.update(engine);
   background("cadetBlue");
 
-  stroke("white")
-  strokeWeight(4)
+  fill("black")
   textSize(30)
   text("Press the Down Arrow to drop the green block", 100,50);
 
-
-  fill("black")
+  strokeWeight(3) 
+  stroke("white")
   rectMode(CENTER)
   rect(ground.position.x,ground.position.y,800,10)
 
-  strokeWeight(3)
+
   fill("lightgreen")
   ellipseMode(RADIUS)
-  ellipse(ball1.position.x, ball1.position.y, 40,40)
+  ellipse(greenBall.position.x, greenBall.position.y, 40,40)
 
-  
+
   rectMode(CENTER)
   fill("brown")
-  rect(square1.position.x, square1.position.y, 70,70)
-  rect(square2.position.x, square2.position.y, 70,70)
-  rect(square3.position.x, square3.position.y, 70,70)
-
+  rect(stone1.position.x, stone1.position.y, 70,70)
+  rect(stone2.position.x, stone2.position.y, 70,70)
+  rect(stone3.position.x, stone3.position.y, 70,70)
+  //draw the rectangle for stone 4
+  //draw the rectangle for stone 5
+  rect(stone6.position.x, stone6.position.y, 70,70)
   
-  rect(square6.position.x, square6.position.y, 70,70)
-  
-
   drawSprites();
 
 }
@@ -75,7 +82,7 @@ function draw() {
 function keyPressed() {
 
 	if (keyCode === DOWN_ARROW) {
-		Matter.Body.setStatic(ball1,true); 
+		Matter.Body.setStatic(greenBall,true); 
 	}
 }
 
